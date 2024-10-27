de47fca266988de49ac2ad35573b967dadce2676
Jishnuc07 <137303708+Jishnuc07@users.noreply.github.com>
2024-10-27 20:28:58 +0530

    Fourth Commit

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app"]
+# ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
